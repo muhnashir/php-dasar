6212b29dff1b864e82595a479a523c3fc1c15c32
muhnashir <muhammadnashir9@gmail.com>
2021-10-04 19:50:34 +0700

    first commit

---
+++ phpDasar1.php
@@ -0,0 +1,111 @@
+<?php
+
+
+function average($nilai){
+    $arrLength = count($nilai);
+    $tmp=0;
+    for($i = 0; $i<$arrLength; $i++){
+        $tmp = $tmp + $nilai[$i];
+    }
+    return $tmp/$arrLength;
+}
+
+function minimum($nilai){
+    $arrLength = count($nilai);
+    $tmp;
+    for($i = 0; $i<$arrLength; $i++){
+        if($i==0){
+            $tmp=$nilai[$i];
+        }else{
+            if($nilai[$i]<$tmp){
+               $tmp=$nilai[$i]; 
+            }
+        }
+    }
+
+    return "Nilai terkecil ".$tmp;
+}
+
+function maksimum($nilai){
+    $arrLength = count($nilai);
+    $tmp;
+    for($i = 0; $i<$arrLength; $i++){
+        if($i==0){
+            $tmp=$nilai[$i];
+        }else{
+            if($nilai[$i]>$tmp){
+               $tmp=$nilai[$i]; 
+            }
+        }
+    }
+
+    return "Nilai terbesar ".$tmp;
+}
+
+function hurufkecil($string){
+    $tmp=0;
+    $kalimat = str_split($string);
+    for($i = 0; $i<count($kalimat); $i++){
+        if(ctype_lower($kalimat[$i])){
+            $tmp++;
+        }
+    }
+    return $string." mengandung ".$tmp. " huruf kecil";
+}
+
+function generate($kalimat){
+    $arrInput = explode(' ', $kalimat);
+	$unigram = '';
+	foreach ($arrInput as $data) {
+		$unigram .= $data.', ';
+	}
+	$unigram = substr($unigram, 0, -2);
+
+	$x = 0;
+	$bigram = '';
+	foreach ($arrInput as $data) {
+		if($x < 1) {
+			$bigram .= $data.' ';
+			$x++;
+		}else {
+			$bigram .= $data.', ';
+			$x = 0;
+		}
+	}
+	$bigram = substr($bigram, 0, -2);
+
+	$y = 0;
+	$trigram = '';
+	foreach ($arrInput as $data) {
+		if($y < 2) {
+			$trigram .= $data.' ';
+			$y++;
+		}else {
+		    $trigram .= $data.', ';
+		    $y = 0;
+		}
+	}
+	$trigram = substr($trigram, 0, -2);
+
+
+	$hasil = 'Unigram : '.$unigram. '<br>';
+	$hasil .= 'Bigram : '.$bigram. '<br>';
+	$hasil .= 'Trigram : '.$trigram;
+
+	return $hasil;
+}
+
+
+// $nilai = [8,8,9,10];
+// echo average($nilai);
+// echo minimum($nilai);
+// echo maksimum($nilai);
+
+// $string = "muHAmmAD NaSHIr";
+// echo hurufkecil($string);
+
+
+$kalimat = "Jakarta adalah ibukota negara Republik Indonesia";
+echo generate($kalimat);
+
+?>
---
+++ phpDasar2.php
@@ -0,0 +1,68 @@
+<?php
+
+  function pola($input){
+    $color['black']  = [1,2,5,7,10,11];
+    $color['white'] = [3,4,6,8,9,12];
+
+    if (in_array($input, $color['black'])) {
+      return 'style="background : black; color: white;"';
+    } else {
+      return 'style="background : white"';
+    }
+  }
+
+  //analisis 
+  // D  F  H  K  N  Q
+  // E  D  K  G  S  K
+  // +1 -2 +3 -4 +5 -6
+  function enkripsi($plaintext){
+
+    $huruf = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    $toUPPER = strtoupper($plaintext);
+    $arrInput = str_split($toUPPER);
+    $arrLength = count($arrInput);
+    $chippertext = "";
+    $genap =true;
+    $tmp;
+    for($i=0; $i<$arrLength; $i++){
+        $tmp = array_search($arrInput[$i], $huruf);
+        if($genap){
+            $chippertext.=$huruf[$tmp+($i+1)];
+            $genap = false;
+        }else{
+            $ne = $tmp-($i+1);
+			if($ne < 0) {
+			    $ne = count($huruf) + ($ne);
+			}
+			$chippertext .= $huruf[$ne];
+			$genap = true;
+        }
+
+    }
+    return $chippertext;
+		
+  } 
+
+//   $no = 1;
+//   $v = 1;
+//   echo "<table>";
+//   for($i = 0; $i < 8; $i++){
+//    echo '<tr>';
+//     for($x = 0; $x < 8; $x++){
+//       echo '<td '.pola($v).'>';
+//       echo $no++;
+//       echo '</td>';
+//       if ($v==12) {
+//         $v = 1;
+//       } else {
+//         $v++;
+//       }
+//     }
+//    echo '</tr>';
+//   }
+//   echo "</table>";
+
+$plaintext = "DFHKNQ";
+echo enkripsi($plaintext);
+
+?>
